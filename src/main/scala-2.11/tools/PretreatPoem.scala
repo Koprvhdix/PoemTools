16625ecbd3cacd2b95ac0cf8574bd8b5f4011066
Koprvhdix <454898060@qq.com>
2016-11-18 17:47:59 +0800

    first commit pretreat

--- src/main/scala-2.11/tools/PretreatPoem.scala
+++ src/main/scala-2.11/tools/PretreatPoem.scala
@@ -1,4 +1,8 @@
-package tools
+import scala.io.Source
+import scala.reflect.io.Directory
+import java.io.File
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
   * Created by wukai on 16-11-13.
@@ -6,6 +10,15 @@ package tools
   */
 object PretreatPoem {
   def main(args: Array[String]): Unit = {
+    val folder = "poem"
+    for (chapter <- new File(folder).listFiles()) {
+      for (file <- new File(chapter.toString).listFiles()) {
+        for (line <- Source.fromFile(file).getLines()) {
+          if (!line.isEmpty) {
 
+          }
+        }
+      }
+    }
   }
 }
