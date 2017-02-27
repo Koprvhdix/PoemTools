1a031b12483c19213b146600870bb1c72236e82c
吴凯的MacBook <koprvhdix@wukaideMacBookdeMacBook-Pro.local>
2017-02-27 11:41:30 +0800

    fix spider

--- spiders/spiders/spiders/spider_quantangshi.py
+++ spiders/spiders/spiders/spider_quantangshi.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from scrapy.spider import Spider
 import scrapy
+from scrapy import log
 import os
 
 
@@ -22,14 +23,18 @@ class SpiderQuanTangPoem(Spider):
             link = sel.xpath('a/@href').extract()
             chapter_url_list.append(link[0])
         # get the poem url list
-        for url in chapter_url_list:
-            yield scrapy.Request(url, callback=self.parse_poem_list, dont_filter=True)
+        # for url in chapter_url_list:
+        yield scrapy.Request(chapter_url_list[0], callback=self.parse_poem_list, dont_filter=True)
 
     def parse_poem_list(self, response):
         """
             get the poem url list
         """
         poem_url_list = list()
+
+        for sel in response.xpath('/html/body/div[1]/center/table/tbody/tr[2]/td[2]/p'):
+            link = sel.xpath('a/@href').extract()
+            yield scrapy.Request(link[0], callback=self.parse_poem_list, dont_filter=True)
 
         for sel in response.xpath('//span'):
             # file_open.writelines(sel.encode('utf-8'))
@@ -44,11 +49,11 @@ class SpiderQuanTangPoem(Spider):
             get the poem
         """
         chapter_poem = response.url.split("/")[-1].split("?")[1].split("&")
-        folder_name = "/home/wukai/Projects/PoemTools/poem/" + chapter_poem[0].split("=")[1]
+        folder_name = "/Users/koprvhdix/Projects/PoemTools/poem/" + chapter_poem[0].split("=")[1]
         if not os.path.exists(folder_name):
             os.makedirs(folder_name)
         file_name = folder_name + "/" + chapter_poem[1].split("=")[1]
-        file_open = open(file_name, 'wb')
+        file_open = open(file_name, 'w')
         text_set = response.xpath('//font/text()').extract()
         for text in text_set:
-            file_open.writelines(text.encode('utf-8'))
+            file_open.writelines(text)
